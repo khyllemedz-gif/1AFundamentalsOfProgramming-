fd226aa41c0f03d118dd952f7ae03f8e7bbaa46e
unknown <khynetics@gmail.com>
2026-09-02 21:49:12 +0800

    sample

---
+++ Assignment3.java
@@ -0,0 +1,31 @@
+public class Assignment3 {
+
+    public static void main(String[] args) {
+
+        String firstname = "Kylle";
+        //String represents the multiple characters that enclosed by double quotes and it contains text.
+        String lastname = "Medrano";
+        //String represents the texts. Using this it can stores the lastname.
+        String address = "Sto.Tomas, Pangasinan";
+        // String represents texts. Using this, it stores the address.
+       long MobileNumber = 639319283036L;
+        // long represents the number is too large for int. use this for mobile numbers.
+        int age = 17;
+        //  int represents whole numbers. use this for storing age.
+        double GWA = 94.595;
+        // double represents when it contains decimal number, use this for storing GWA.
+
+        System.out.println("Firstname: " + firstname);
+        // Displays the first name.
+        System.out.println("Lastname: " + lastname);
+        // Displays the last name.
+        System.out.println("Address: " + address);
+        // Displays the address.
+        System.out.println("Mobile Number: " + MobileNumber);
+        // Displays the Mobile Number.
+        System.out.println("Age: " + age);
+        // Displays the age.
+        System.out.println("GWA: " + GWA);
+        // Displays the GWA.
+    }
+}
