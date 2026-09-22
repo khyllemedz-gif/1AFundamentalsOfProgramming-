a7f6214e171ae8c247ec91090366cbc1af630317
Kylle <khynetics@gmail.com>
2026-09-22 22:28:02 +0800

    Add files via upload

---
+++ firstJava.java
@@ -0,0 +1,13 @@
+public class firstJava {
+    public static void main(String[] args) {
+
+        // Hero
+        System.out.println("Hero Jerico Cantor Oliver");
+        System.out.println("BSIT-1A");
+
+        // Kylle
+        System.out.println("Kylle Hamir Moreno Medrano");
+        System.out.println("BSIT-1A");
+
+    }
+} 
